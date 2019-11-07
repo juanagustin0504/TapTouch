3cef1c5f4ae2dcf95e6775fb9b4af58f2b63a61d
juanagustin0504 <juanagustin@naver.com>
2019-11-07 17:18:25 +0900

    add : tap/touch

--- TapTouch/ViewController.swift
+++ TapTouch/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var txtMessage: UILabel!
+    @IBOutlet weak var txtTapsLevel: UILabel!
+    @IBOutlet weak var txtTouchesLevel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        txtMessage.text = "Touches Began"
+        txtTapsLevel.text = String(touch.tapCount)
+        txtTouchesLevel.text = String(touches.count)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Moved"
+        txtTapsLevel.text = String(touch.tapCount)
+        txtTouchesLevel.text = String(touches.count)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        txtMessage.text = "Touches Ended"
+        txtTapsLevel.text = String(touch.tapCount)
+        txtTouchesLevel.text = String(touches.count)
     }
 
 
